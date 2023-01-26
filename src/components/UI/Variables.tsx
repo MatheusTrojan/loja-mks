4c8862dba0d58cf1d759c2b72f5c20833cefbcef
Matheus Trojan <matheustrojan23@gmail.com>
2023-01-26 09:33:17 -0300

    Configura cores padrão e globalstyles

---
+++ src/components/UI/Variables.tsx
@@ -0,0 +1,10 @@
+export const primaryColor = "#0F52BA";
+export const secondaryColor = "#E5E5E5";
+export const tertiaryColor = "#373737";
+export const footerColor = "#EEEEEE";
+
+export const fontColor = "#2C2C2C";
+
+export const white = "#FFFFFF";
+export const black = "#000000";
+
