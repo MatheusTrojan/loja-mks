946905738e5cff214af669ea817985fff4cf1827
Matheus Trojan <matheustrojan23@gmail.com>
2023-01-31 15:55:34 -0300

    cria testes para as funçoes dos slices

--- src/store/slices/cartSlice.test.ts
+++ src/store/slices/cartSlice.test.ts
@@ -1,4 +1,16 @@
-import cartReducer, { CartState, addToCart, removeFromCart, updateQuantity, addItemQuantity, subItemQuantity } from "./cartSlice"
+import cartReducer, { 
+    CartState,
+    addToCart,
+    removeFromCart, 
+    updateQuantity, 
+    addItemQuantity, 
+    subItemQuantity, 
+    getNumItems,
+    getMemoizedNumItems,
+    getTotalPrice
+} from "./cartSlice"
+import products from "../../../public/products.json"
+import type { RootState } from "../../store";
 
 describe("cart reducer", () => {
 
@@ -80,4 +92,164 @@ describe("cart reducer", () => {
             items: { abc: 4, def: 1 } 
         })
     });
+})
+
+describe("selectors", () => {
+
+    describe("getNumItems", () => {
+
+        it("should return 0 with no items", () => {
+            const cart: CartState = {
+                items: {}
+            }
+            const result = getNumItems({ cart } as RootState);
+            expect(result).toEqual(0);
+        })
+
+        it("should add up the total", () => {
+            const cart: CartState = {
+                items: { abc: 3, def: 3, ghi: 6 }
+            }
+            const result = getNumItems({ cart } as RootState);
+            expect(result).toEqual(12);
+        })
+    });
+
+    describe('getMemoizedNumItems', () => {
+
+        it("should return 0 with no items", () => {
+            const cart: CartState = {
+                items: {}
+            }
+            const result = getMemoizedNumItems({ cart } as RootState);
+            expect(result).toEqual(0);
+        })
+
+        it("should add up the total", () => {
+            const cart: CartState = {
+                items: { abc: 3, def: 3, ghi: 6 }
+            }
+            const result = getMemoizedNumItems({ cart } as RootState);
+            expect(result).toEqual(12);
+        })
+
+        it("should not compute again with the same state", () => {
+            const cart: CartState = {
+                items: { abc: 3, def: 3, ghi: 6 }
+            };
+            getMemoizedNumItems.resetRecomputations();
+
+            getMemoizedNumItems({ cart } as RootState);
+            expect(getMemoizedNumItems.recomputations()).toEqual(1);
+            getMemoizedNumItems({ cart } as RootState);
+            getMemoizedNumItems({ cart } as RootState);
+            expect(getMemoizedNumItems.recomputations()).toEqual(1);
+        })
+
+        it("should recompute with new state", () => {
+            const cart: CartState = {
+                items: { abc: 3, def: 3, ghi: 6 }
+            };
+            getMemoizedNumItems.resetRecomputations();
+
+            getMemoizedNumItems({ cart } as RootState);
+            expect(getMemoizedNumItems.recomputations()).toEqual(1);
+
+            cart.items = { abc: 2 }
+            getMemoizedNumItems({ cart } as RootState);
+            expect(getMemoizedNumItems.recomputations()).toEqual(2);
+        })
+    }),
+
+    describe('getTotalPrice', () => {
+
+        it("should return zero with an empty cart", () => {
+            const state: RootState = {
+                cart: { items: {} },
+                products: {products: {}}
+            }
+            const result = getTotalPrice(state)
+            expect(result).toEqual(0)
+        });
+
+        it("should add up the total", () => {
+            const state: RootState = {
+                cart: { items: {
+                    [products[0].id]: 3,
+                    [products[1].id]: 4
+                } },
+                products: {products: {
+                    [products[0].id]: products[0],
+                    [products[1].id]: products[1]
+                }}
+            }
+            const result = getTotalPrice(state);
+            expect(result).toEqual(19800);
+        });
+
+        it("should compute again with the same state", () => {
+            const state: RootState = {
+                cart: { items: {
+                    [products[0].id]: 3,
+                    [products[1].id]: 4
+                } },
+                products: {products: {
+                    [products[0].id]: products[0],
+                    [products[1].id]: products[1]
+                }}
+            }
+            getTotalPrice.resetRecomputations();
+            const result = getTotalPrice(state);
+            expect(result).toEqual(19800);
+            expect(getTotalPrice.recomputations()).toEqual(1);
+            getTotalPrice(state);
+            expect(getTotalPrice.recomputations()).toEqual(1);
+        });
+
+        it("should recompute with new products", () => {
+            const state: RootState = {
+                cart: { items: {
+                    [products[0].id]: 3,
+                    [products[1].id]: 4
+                } },
+                products: {products: {
+                    [products[0].id]: products[0],
+                    [products[1].id]: products[1]
+                }}
+            }
+            getTotalPrice.resetRecomputations();
+            let result = getTotalPrice(state);
+            expect(result).toEqual(19800);
+            expect(getTotalPrice.recomputations()).toEqual(1);
+            state.products.products = {
+                [products[0].id]: products[0],
+                [products[1].id]: products[1],
+                [products[2].id]: products[2]
+            }
+            result = getTotalPrice({ ...state });
+            expect(result).toEqual(19800);
+            expect(getTotalPrice.recomputations()).toEqual(2);
+        });
+
+        it("should recompute when cart changes", () => {
+            const state: RootState = {
+                cart: { items: {
+                    [products[0].id]: 3,
+                    [products[1].id]: 4
+                } },
+                products: {products: {
+                    [products[0].id]: products[0],
+                    [products[1].id]: products[1]
+                }}
+            }
+            getTotalPrice.resetRecomputations();
+            let result = getTotalPrice(state);
+            expect(result).toEqual(19800);
+            expect(getTotalPrice.recomputations()).toEqual(1);
+            state.cart.items = {}
+            result = getTotalPrice({ ...state });
+            expect(result).toEqual(0);
+            expect(getTotalPrice.recomputations()).toEqual(2);
+        });
+    })
 })
--- src/store/slices/productsSlice.test.ts
+++ src/store/slices/productsSlice.test.ts
@@ -1,6 +1,8 @@
 import productsReducer, { receivedProducts } from "./productsSlice";
+import products from "../../../public/products.json"
 
 describe("products reducer", () => {
+
     it("should return the initial state when passed an empty action", () => {
         const initalState = undefined;
         const action = { type: "" };
@@ -8,4 +10,38 @@ describe("products reducer", () => {
         expect(result).toEqual({ products: {} })
     });
 
-})
+    it("should convert the products received to an object", () => {
+        const initalState = undefined;
+        const action = receivedProducts(products);
+        const result = productsReducer(initalState, action);
+        expect(Object.keys(result.products).length).toEqual(products.length)
+        products.forEach((product) => {
+            expect(result.products[product.id]).toEqual(product);
+        });
+    });
+
+    it("should not allow the same product to be added more than once", () => {
+        const initalState = undefined;
+        const action = receivedProducts(products);
+        let result = productsReducer(initalState, action);
+        expect(Object.keys(result.products).length).toEqual(products.length)
+        products.forEach((product) => {
+            expect(result.products[product.id]).toEqual(product);
+        });
+
+        result = productsReducer(result, action);
+        expect(Object.keys(result.products).length).toEqual(products.length)
+    });
+    
+    it("should allow multiple products to be received at different times", () => {
+        const initalState = undefined;
+        const action = receivedProducts(products.slice(0, 2));
+        let result = productsReducer(initalState, action);
+        expect(Object.keys(result.products).length).toEqual(2)
+
+        const secondAction = receivedProducts(products.slice(2, 4));
+        result = productsReducer(result, secondAction);
+
+        expect(Object.keys(result.products).length).toEqual(4)
+    });
+});
